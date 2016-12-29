5dda68abccdd74bea94b61aa48bd85cfc1e7f6b0
Atolagbe Abisoye <atolagbe.bisoye@andela.com>
2016-12-29 13:50:45 +0100

    Add console.logs

--- server/routes/index.js
+++ server/routes/index.js
@@ -10,6 +10,7 @@ var storage = multer.diskStorage({
   },
   filename: function(req, file, cb) {
 	var date = new Date()
+   console.log('FILLLEE USERNAME', req.body.username)
     cb(null, req.body.username + '-' + date);
   }
 });
